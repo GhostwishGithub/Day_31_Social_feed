14061a44b801d3d13a8648df070f5e320a2579c0
unknown <bensandlin2@gmail.com>
2022-09-15 13:56:28 -0500

    Realized the page needs to NOT update everytime a new input is detected: quickly added that functionality inside CreatePost.jsx

--- src/Components/DisplayPost/CreatePost/CreatePost.jsx
+++ src/Components/DisplayPost/CreatePost/CreatePost.jsx
@@ -4,14 +4,19 @@ import React, { useState } from 'react';
 const CreatePost = (props) => {
 
     const [name, setName] = useState('');
-    const [post, setDate] = useState('')
+    const [post, setDate] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+    }
 
     return (
         <form>
             <label>Name</label>
             <input type='string' value={name} onChange={(event) => setName(event.target.value)} />
             <label>Post</label>
-            <input type='string' value={post} />
+            <input type='string' value={post} onChange={(event) => setDate(event.target.value)} />
+            <button type='submit'>Create</button>
         </form>
       );
 }
